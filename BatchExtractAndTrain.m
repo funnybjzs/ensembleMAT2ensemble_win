6e52386702ce363519fd2321e07a4255b8edea40
zhaoshuo <funnybjzs89@gmail.com>
2016-11-14 21:07:08 +0800

    v1.1

--- BatchExtractAndTrain.m
+++ BatchExtractAndTrain.m
@@ -33,23 +33,34 @@ for i=1:length(var.stego_algo)
 end
 
 mdl_save_path=[outputdir,'\',input.image_type,'.mdl'];
+results=zeros(1,length(stego_img_path));
 
 % get features
-if(strfind(input.image_type,'jpg')) %jpeg part
-    system(['.\DCTR_COMMAND\cuda_test_DCTRV12 ', cover_img_path{1},' ',cover_feature_file{1},' ',cover_size_file{1},' ',cover_quantable_file{1},' ','names.txt']);
+if(strfind(input.image_type,'jpg')) %jpeg parts
+    %system(['.\DCTR_COMMAND\cuda_test_DCTRV12 ', cover_img_path{1},' ',cover_feature_file{1},' ',cover_size_file{1},' ',cover_quantable_file{1},' ','names.txt']);
     %stego
-    for stego_count=1:length(stego_img_path)
-    system(['.\DCTR_COMMAND\cuda_test_DCTRV12 ', stego_img_path{stego_count},' ',stego_feature_file{stego_count},' ',stego_size_file{stego_count},' ',stego_quantable_file{stego_count},' ','names.txt']);
+    while(length(find(results==1))~=length(var.stego_algo)*length(var.stego_payload))
+        for stego_count=1:length(stego_img_path)
+             if(numel(dir([stego_img_path{stego_count},'\*.jpg']))==12194&&results(stego_count)==0)
+                system(['.\DCTR_COMMAND\cuda_test_DCTRV12_v7.5 ', stego_img_path{stego_count},' ',stego_feature_file{stego_count},' ',stego_size_file{stego_count},' ',stego_quantable_file{stego_count},' ','names.txt']);
+                results(stego_count)=1;
+            end
+        end
     end
-else % spatial part
-    system(['.\SRM_COMMAND\cuda_test_SRM12 ', cover_img_path{1},' ',cover_feature_file{1},' ',cover_size_file{1},' ','names.txt']);
-    %stego
-    for stego_count=1:length(stego_img_path)
-    system(['.\SRM_COMMAND\cuda_test_SRM12 ', stego_img_path{stego_count},' ',stego_feature_file{stego_count},' ',stego_size_file{stego_count},' ','names.txt']);
+else % spatial parts
+    %system(['.\SRM_COMMAND\cuda_test_SRM12 ', cover_img_path{1},' ',cover_feature_file{1},' ',cover_size_file{1},' ','names.txt']);
+    %stego    
+    while(length(find(results==1))~=length(var.stego_algo)*length(var.stego_payload))
+        for stego_count=1:length(stego_img_path)
+             if(numel(dir([stego_img_path{stego_count},'\*.jpg']))==12194&&results(stego_count)==0)
+                system(['.\cuda_test_SRM12_v7.5 ', stego_img_path{stego_count},' ',stego_feature_file{stego_count},' ',stego_size_file{stego_count},' ','names.txt']);
+                results(stego_count)=1;
+             end             
+        end
     end
 end
 
-[result_type,trained_ensemble]=ensembleMAT2ensemble(cover_feature_file,cover_quantable_file,cover_size_file,stego_feature_file,stego_quantable_file,stego_size_file,mdl_save_path);
-fprintf([result_type{end},'\n\n\n\n']);
+% [result_type,trained_ensemble]=ensembleMAT2ensemble(cover_feature_file,cover_quantable_file,cover_size_file,stego_feature_file,stego_quantable_file,stego_size_file,mdl_save_path);
+% fprintf([result_type{end},'\n\n\n\n']);
 
 end
--- mytest.m
+++ mytest.m
@@ -1,11 +1,24 @@
-input.image_type='jpg_75_1500x1000';
 input.image_path='Z:\';
+image_type='jpg';
+image_size='320x240';
+qf_start=70;
+qf_end=100;
+%extract cover
+var.outpath='Z:\';
+var.stego_algo={'null'};
+var.stego_payload={'000'};
+for i=qf_start:qf_end
+    input.image_type=[image_type,'_',num2str(i),'_',image_size];
+    CoverBatchExtrat(input,var);
+end
 
-var.outpath='D:\zhaoshuo\';
-%read only
-var.stego_algo={'F5','MME','JLSBM','JSteg','NSF5'};
-var.stego_payload={'200','300','400'};
-BatchExtractAndTrain(input,var);
+%extract stego
+% for i=qf_start:qf_end
+%     input.image_type=[input.image_type,'_',num2str(i)];
+%     var.stego_algo={'F5','MME','JLSBM','JSteg','NSF5'};
+%     var.stego_payload={'200','300','400'};
+%    BatchExtractAndTrain(input,var);
+% end
 
 % input.image_type='png_1500x1000';
 % input.image_path='Z:\';
